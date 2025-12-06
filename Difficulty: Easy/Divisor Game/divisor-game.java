28d87b88dafdf1485bd2f0de981bf133b1527c07
Ujjwal Kriti <tanukriti06@gmail.com>
2025-12-06 10:00:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Divisor Game/divisor-game.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        if(n%2==0){
+            return true;
+        }else
+         return false;
+    }
+}
