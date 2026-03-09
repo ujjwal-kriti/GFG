748ba30f46f103c59bc9c8f3e76fbb0fcfd178a8
Ujjwal Kriti <tanukriti06@gmail.com>
2026-03-09 13:08:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
@@ -3,19 +3,23 @@
 class Solution {
     public void segregateElements(int[] arr) {
         // Your code goes here
-        int n=arr.length;
-        int [] temp=new int[n];
         int idx=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>=0)
-            temp[idx++]=arr[i];
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]<0)
-            temp[idx++]=arr[i];
-        }
-        for(int i=0;i<n;i++){
-            arr[i]=temp[i];
-        }
+      int n=arr.length;
+      int[] temp=new int[n];
+      for(int i=0;i<n;i++){
+          if(arr[i]>=0){
+              temp[idx]=arr[i];
+              idx++;
+          }
+      }
+       for(int i=0;i<n;i++){
+          if(arr[i]<0){
+              temp[idx]=arr[i];
+              idx++;
+          }
+      }
+       for(int i=0;i<n;i++){
+         arr[i]=temp[i];
+      }
     }
 }
