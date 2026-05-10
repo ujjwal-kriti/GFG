684245df7eff23595b0c4aff4d65ca352d788eab
Ujjwal Kriti <tanukriti06@gmail.com>
2026-05-10 15:50:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -1,16 +1,24 @@
 class Solution {
     // Function to calculate the number of subsets with a given sum
     public int perfectSum(int[] nums, int target) {
-       int[] dp = new int[target + 1];  
-
-        dp[0] = 1;  
-
-        for (int num : nums) {  
-            for (int j = target; j >= num; j--) {  
-                dp[j] += dp[j - num];  
-            }  
-        }  
-
-        return dp[target];  
+        int n=nums.length;
+        
+        int[][] dp=new int [n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                if(nums[i-1]<=j){
+                    int take=dp[i-1][j-nums[i-1]];
+                    int not_take=dp[i-1][j];
+                    dp[i][j]=take+not_take;
+                    
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
     }
 }
