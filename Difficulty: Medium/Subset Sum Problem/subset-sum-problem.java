36eb37baec8c8b334262d37c7ed394ad15a39c76
Ujjwal Kriti <tanukriti06@gmail.com>
2026-05-10 10:46:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,36 +1,24 @@
 class Solution {
 
-     public static Boolean isSubsetSum(int arr[], int sum) {
-        // ----------------reursion---------------
-    //     return  helper(arr,arr.length,sum);
-    // }
-    // public static  boolean helper(int arr[],int i,int sum){
-    //     if(sum==0) return true;
-    //     if(i==0) return false;
-    //     if(arr[i-1]<=sum){
-    //         return helper(arr,i-1,sum) || helper(arr,i-1,sum-arr[i-1]);
-    //     }
-    //     return helper(arr,i-1,sum);
-    
-        
-    //---------------------------------tabulationmethod----------------
-        
-        int n=arr.length;
-        boolean[][] dp=new boolean[n+1][sum+1];
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        for(int i=1;i<=n;i++){
-            for(int s=1;s<=sum;s++){
-                dp[i][s]=dp[i-1][s];
-                if(arr[i-1]<=s){
-                    dp[i][s]=dp[i][s]||dp[i-1][s-arr[i-1]];
-                }
+    static Boolean isSubsetSum(int arr[], int sum) {
+     int n=arr.length;
+    boolean[][] dp=new boolean[n+1][sum+1];
+    for(int i=0;i<=n;i++){
+        dp[i][0]=true;
+    } 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(arr[i-1]<=j){
+                boolean take=dp[i-1][j-arr[i-1]];
+                boolean  not_take=dp[i-1][j];
+                dp[i][j]=take||not_take;
+               
+            }else{
+                dp[i][j]=dp[i-1][j];
             }
         }
-        
-        return dp[n][sum];
-        
-        
+    }
+    return dp[n][sum];
+    
     }
 }
