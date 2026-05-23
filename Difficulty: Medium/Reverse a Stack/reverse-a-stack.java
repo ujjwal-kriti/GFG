f416fa451a03d88f5ff4a8d7e5b32b6b85a08ac8
Ujjwal Kriti <tanukriti06@gmail.com>
2026-05-23 22:38:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
@@ -1,18 +1,20 @@
 class Solution {
     public static void reverseStack(Stack<Integer> st) {
         // code here
-        if(st.isEmpty()) return;
-        int top=st.pop();
-        reverseStack(st);
-        pushAtBottom(st,top);
-    }
-    private  static void pushAtBottom(Stack<Integer>st,int x){
         if(st.isEmpty()){
-            st.push(x);
             return;
         }
         int top=st.pop();
-        pushAtBottom(st,x);
+        reverseStack(st);
+        insertAtBottom(st,top);
+    }
+    public static void insertAtBottom(Stack<Integer>st,int elmnt){
+        if(st.isEmpty()){
+            st.push(elmnt);
+            return;
+        }
+        int top=st.pop();
+        insertAtBottom(st,elmnt);
         st.push(top);
     }
 }
