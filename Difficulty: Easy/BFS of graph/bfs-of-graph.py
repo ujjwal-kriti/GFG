13f19082637f69968a348dc579a7ca43e7251ec6
Ujjwal Kriti <tanukriti06@gmail.com>
2026-06-13 10:14:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.py
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.py
@@ -1,16 +1,23 @@
+
+
 class Solution:
     def bfs(self, adj):
-        # code here
-        n=len(adj)
-        visited=[False]*n
-        q=deque([0])
-        visited[0]=True
-        ans=[]
+        n = len(adj)
+
+        visited = [False] * n
+        q = deque([0])
+
+        visited[0] = True
+
+        ans = []
+
         while q:
-            node=q.popleft()
+            node = q.popleft()
             ans.append(node)
-            for nei  in adj[node]:
+
+            for nei in adj[node]:
                 if not visited[nei]:
-                    visited[nei]=True
+                    visited[nei] = True
                     q.append(nei)
+
         return ans
